a4573a5f0679d83d00d3cc785e3ececaf93115b6
Leibril <ariabi.sr@gmail.com>
2025-02-04 16:47:16 -0600

    Creación del diseño del header

--- components/header/header.js
+++ components/header/header.js
@@ -2,6 +2,17 @@ function header(){
 
     let header = document.createElement('header');
     header.className = "header";
+
+    let logo = document.createElement('img');
+    logo.src = "https://infouniversitarios.examtime.com/files/2014/11/Apunres-Universitarios.png";
+    logo.alt = "logo";
+
+    header.appendChild(logo);
+
+    let textH1 = document.createElement('h1');
+    textH1.textContent = "Todo-List";
+    header.appendChild(textH1);
+
     return header;
 
 }
