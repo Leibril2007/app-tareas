9921de8df8eeff562f28314bb9b280b532886901
Leibril <ariabi.sr@gmail.com>
2025-02-07 16:43:54 -0600

    Diseño de Formulario

--- components/formulario/formulario.js
+++ components/formulario/formulario.js
@@ -2,6 +2,19 @@ function formulario(){
     let form = document.createElement('div');
     form.className = "d-formulario";
 
+
+    let formulario = document.createElement('input');
+    formulario.className = "input-formulario";
+    formulario.type = 'text';
+    formulario.placeholder = 'Write a task...';
+
+    let boton = document.createElement('div');
+    boton.className = "boton-form";
+    boton.textContent = "Add";
+
+    form.appendChild(formulario);
+    form.appendChild(boton);
+
     return form;
 }
 
