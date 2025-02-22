38cfa4165399ff9db7536c727168a2866129c200
Leibril <ariabi.sr@gmail.com>
2025-02-21 20:47:17 -0600

    Actualizaciones - Se reorganiza el sistema de carpetas. - Se crea la función agregarTarea().

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-//IMPORTAR COMPONENTES
 import { header } from "./components/header/header.js";
-import { tareas } from "./components/tarea/tarea.js";
+import { cargarTareas } from "./components/tarea/tarea.js";
 import { formulario } from "./components/formulario/formulario.js";
+
 
 let DOM = document.getElementById("root");
 
-DOM.appendChild(header());
-DOM.appendChild(tareas());
-DOM.appendChild(formulario());
+function cargarDOM(){
+
+    DOM.appendChild(header());
+    
+    let contenedorTareas = document.createElement('div');
+    contenedorTareas.id = 'contenedor-tareas';
+    contenedorTareas.appendChild(cargarTareas());
+    
+    DOM.appendChild(contenedorTareas);
+    DOM.appendChild(formulario());
+       
+}
+
+cargarDOM();
+
+
+
+
+
+
 
